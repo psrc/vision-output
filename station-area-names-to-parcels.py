187b9311933aa54d1b0aad4c12a76716d84e0f5f
chelmann <chelmann@psrc.org>
2019-10-03 08:53:27 -0700

    revamped scenario comparison

--- station-area-names-to-parcels.py
+++ station-area-names-to-parcels.py
@@ -2,14 +2,15 @@ import pandas as pd
 import os
 import geopandas as gp
 
+geo = "regional_growth_center"
 working_directory = os.getcwd()
 input_directory = os.path.join(working_directory, 'inputs','shapefiles')
 output_directory = os.path.join(working_directory, 'inputs')
 
 # Shapefiles
 parcel_file = os.path.join(input_directory,'parcel_centroids.shp')
-lrt_stations = os.path.join(input_directory,'lrt_station_areas.shp')
-lrt_projection = os.path.join(input_directory,'lrt_station_areas.prj')
+poly_file = os.path.join(input_directory,'urbcen.shp')
+poly_projection = os.path.join(input_directory,'urbcen.prj')
 state_plane = 'epsg:2285'
 
 # Spatial Joing Function
@@ -30,20 +31,20 @@ def gp_spatial_join(target_shapefile,join_shapefile,coord_sys,keep_columns):
     
     return merged
 
-print('Join the Parcels and Light Rail Station Area Buffers')
-columns_to_keep = ['PARCELID','stop_name']
-parcel_stations = gp_spatial_join(parcel_file, lrt_stations, state_plane, columns_to_keep )
-updated_names = ['parcel_id','station_name']
-parcel_stations.columns = updated_names               
+print('Join the Parcels and Polygon Buffers')
+columns_to_keep = ['PARCELID','NAME']
+poly_joins = gp_spatial_join(parcel_file, poly_file, state_plane, columns_to_keep )
+updated_names = ['parcel_id','rgc_name']
+poly_joins.columns = updated_names               
 
-print('Create a full Parcel file to merge station names with and fill NaN with a blank value')
+print('Create a full Parcel file to merge ploygon names with and fill NaN with a blank value')
 parcels_df = gp.GeoDataFrame.from_file(parcel_file)
 columns_to_keep = ['PARCELID']
 parcels = parcels_df.loc[:,columns_to_keep]
 updated_names = ['parcel_id']
 parcels.columns = updated_names 
-parcels = pd.merge(parcels, parcel_stations, on='parcel_id',suffixes=('_x','_y'),how='left')
+parcels = pd.merge(parcels, poly_joins, on='parcel_id',suffixes=('_x','_y'),how='left')
 parcels.fillna("",inplace=True)
 
 print('Output CSV file to disk')
-parcels.to_csv(os.path.join(output_directory, 'parcels_w_lrt_station_names.csv'),index=False) 
+parcels.to_csv(os.path.join(output_directory, 'parcels_w_' + geo + '_names.csv'),index=False) 
---
+++ trip-summary.py
@@ -0,0 +1,43 @@
+﻿# This script creates a parcel summary csv file for a specific shapefile
+# Created by Puget Sound Regional Council Staff
+# May 2018
+
+import pandas as pd
+import os
+
+working_scenario = 'quick_kick'
+working_directory = os.getcwd()
+input_directory = os.path.join(working_directory,'inputs',working_scenario)
+
+print("Reading the " + working_scenario + " scenario into a pandas dataframe")
+trip_file = os.path.join(input_directory,'_trip.tsv')
+trips = pd.read_csv(trip_file, sep = '\t')
+
+trip_ends = ['opcl','dpcl']
+parcels = []
+i=0
+
+for ends in trip_ends:
+    print("Creating and outputting the " + working_scenario + " scenario " + ends + " by mode and purpose")
+    keep_columns = [ends,'dpurp','mode']
+    w_trips = trips.loc[:,keep_columns]
+    w_trips['trips'] = 1
+
+    w_parcels = w_trips.groupby([ends,'dpurp','mode']).sum()
+    w_parcels = w_parcels.reset_index()
+    w_parcels.columns = ['parcel_id','purpose','mode','trips']
+    w_parcels['type'] = ends
+    w_parcels['scenario'] = working_scenario
+    
+    if i==0:
+        parcels = w_parcels
+        
+    else:
+        parcels = parcels.append(w_parcels,ignore_index=True)
+        
+    i = i + 1
+        
+print("Outputting the final trip file to csv")
+parcels.to_csv(os.path.join(input_directory,'trips_by_mode_and_purpose.csv'),index=False)
+
+
